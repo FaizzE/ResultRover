800b854b19fc0112554cd72b51cd37c5ba33285a
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-10-04 18:46:45 +0530

    Add files via upload

---
+++ page14/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.key === 'u') || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+    }
+});
+
+setInterval(function() {
+    if (window.devtools && window.devtools.open) {
+        window.location.replace("about:blank");
+    }
+}, 1000);
